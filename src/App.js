2068661e8f95a8888f05ffe14aa312702571fcd8
Shekhar saini <sainishekhar581@gmail.com>
2021-10-07 16:43:36 +0530

    Generate paragraph

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import data from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(count<=0){
+      setCount(1);
+      setText(data.slice(0, 1));
+      return;
+    }
+    else if(count>8){
+      setCount(8);
+      setText(data.slice(0, 8));
+      return;
+    }
+    setText(data.slice(0, count));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-header">
+        <h3 className="heading">TIRED OF BORING LOREM IPSUM?</h3>
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="para-input" className="para-label">Paragraph:</label>
+          <input type="number" id="para-input" value={count} onChange={(e)=>setCount(e.target.value)} className="para-input" />
+          <button type="submit" className="btn-generate">Generate</button>
+        </form>
+        <article className="generated-para">
+          {
+            text.map((item,index)=><p key={index}>{item}</p>)
+          }
+        </article>
+      </section>
     </div>
   );
 }
